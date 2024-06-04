8981f7abe25125d37b507a00cf967695d020636b
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2024-06-04 22:38:15 +0600

    student structure created

--- student_management_system.c
+++ student_management_system.c
@@ -1,6 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main () {
+struct student
+{
+    int roll;
+    char first_name[50];
+    char last_name[50];
+    char department[50];
+    char courses[50];
+    char semester[50];
+    char section[50];
+} students[50];
+
+int main()
+{
     printf("<== Student Management System ==>\n");
     return 0;
 }
