9d73d2dea7f09207b306470b331128b5fe66d2b0
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2024-06-04 22:59:32 +0600

    create ui & initial system

--- student_management_system.c
+++ student_management_system.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 struct student
 {
     int roll;
@@ -13,6 +13,59 @@ struct student
 
 int main()
 {
-    printf("<== Student Management System ==>\n");
+    int input;
+
+    while (1)
+    {
+        system("cls");
+        printf("<== Student Management System ==>\n");
+        printf("1.Add Student\n");
+        printf("2.Student List\n");
+        printf("3.Find By Roll\n");
+        printf("4.Find By First Name\n");
+        printf("5.Total Students\n");
+        printf("6.UPdate Student\n");
+        printf("7.Delete Student\n");
+        printf("0.Exit\n\n");
+        printf("Enter your choice: ");
+        scanf("%d", &input);
+
+        switch (input)
+        {
+        case 1:
+            printf("Add student");
+            break;
+
+        case 2:
+            printf("Add student");
+            break;
+
+        case 3:
+            printf("Add student");
+            break;
+
+        case 4:
+            printf("Add student");
+            break;
+
+        case 5:
+            printf("Add student");
+            break;
+
+        case 6:
+            printf("Add student");
+            break;
+
+        case 7:
+            printf("Add student");
+            break;
+
+        default:
+            printf("Invalid Choice...\n\n");
+            break;
+        }
+        printf("\n\nPress Any Key To Continue...");
+    }
+
     return 0;
 }
